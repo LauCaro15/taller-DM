b833074ab46aeb46ef93a9828d231f00de1961f6
LauCaro15 <lccandamil@gmail.com>
2023-10-24 23:13:08 -0500

    Implementation of multiple image uploads

--- models/post.js
+++ models/post.js
@@ -1,26 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-
-const postShema = mongoose.Schema({
-    title: { type: String, require: true },
-    subtitle: { type: String, require: true }, 
-    description: { type: String, require: true },
-    avatar: { type: String, require: true },
+const postSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    subtitle: { type: String, required: true },
+    description: { type: String, required: true },
+    avatar: [{ type: String }],  // Almacena múltiples rutas de imágenes en un arreglo
     active: { type: Boolean, default: true },
-})
+});
 
-module.exports = mongoose.model("Post", postShema);
-
-// const mongoose = require('mongoose')
-
-// const userSchema = mongoose.Schema({
-//     name: { type: String },
-//     lastname: { type: String },
-//     email: { type: String },
-//     password: { type: String },
-//     active: { type: Boolean },
-//     rol: { type: String },
-//     // address: { type: mongoose.Schema.Types.ObjectId, ref: "Address" }
-// });
-
-// module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Post", postSchema);
